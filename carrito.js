40268b80f65824098e75755a8d529315942c9fbd
virginia.leguizamon <leguizamonv500@gmail.com>
2022-01-24 17:55:11 -0300

    ultima modificacion carrito js

--- carrito.js
+++ carrito.js
@@ -26,7 +26,16 @@ alert("Precio total de los productos: " + total)
 let continuar = prompt ("¿Desea continuar comprando?")
 
 if (continuar=="si"){
-    alert=("Continua tu compra " + Producto);
+    
+    let ingresarProductos = parseInt( prompt("Cantidad de productos a llevar") )
+    let total = 0
+     for (let p = 0; p < ingresarProductos ; p++) {
+        const producto = new Producto ( prompt("Ingrese el nombre del producto"),prompt("Precio"))    
+        total += producto.precio
+    }
+        
+    alert("Precio total de los productos: " + total)
 }else{
     alert("¡Gracias por visitarnos!");
 }
+    
